b49586a477ebe784c8c8a803a8a985f3c2f365f4
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-10-15 18:05:28 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const labels = [
+  "Dining",
+  "Entertainment",
+  "Groceries",
+  "Healthcare",
+  "Rent",
+  "Shopping",
+  "Transportation",
+];
+
+const expenseData = labels.map((label) => {
+  const total = Array.from(
+    document.querySelectorAll(`.expense.${label.toLowerCase()}`)
+  ).reduce((sum, item) => {
+    const amountText = item.querySelector("span").innerText;
+    const amount = parseFloat(amountText.replace("$", "").replace(",", ""));
+    return sum + amount;
+  }, 0);
+  return total;
+});
+
+console.log(expenseData);
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "Value Spent",
+        data: expenseData,
+        borderWidth: 1,
+        backgroundColor: [
+          "#abf5fa",
+          "#d9abfa",
+          "#fff698",
+          "#ff9898",
+          "#98c6ff",
+          "#ffc398",
+          "#a2ff98",
+        ],
+      },
+    ],
+  },
+});
