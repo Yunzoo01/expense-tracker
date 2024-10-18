62aa1526a34f8fc9ae854be4afc1801d665d4ff2
Phoebe <a01065241679@gmail.com>
2024-10-17 18:14:40 -0700

    17 Oct summary page layout

--- script.js
+++ script.js
@@ -45,3 +45,70 @@ new Chart(ctx, {
     ],
   },
 });
+
+// Modal for Summary
+var sumModal = document.getElementById("sumModal");
+var sumBtn = document.getElementById("summary");
+var sumSpan = document.getElementsByClassName("close")[0];
+
+sumBtn.onclick = function () {
+  sumModal.style.display = "block";
+}
+
+sumSpan.onclick = function () {
+  sumModal.style.display = "none";
+}
+
+window.onclick = function (e) {
+  if (e.target == sumModal) {
+    sumModal.style.display = "none";
+  }
+}
+
+// Category Selection
+var sumCategory = document.getElementById('category-summary');
+var expenseSummaryContainer = document.getElementById('expense-summary');
+
+sumCategory.addEventListener('change', function () {
+  var selectedCategory = sumCategory.value;
+  console.log("Selected category:", selectedCategory);
+
+  const expenseItems = document.querySelectorAll(`li.expense.${selectedCategory}`);
+
+  // remove previous infos
+  expenseSummaryContainer.innerHTML = "";
+
+  let sumTotalAmount = 0;
+
+  if (expenseItems.length > 0) {
+    expenseItems.forEach(function (expenseItem) {
+      const title = expenseItem.querySelector('h3').textContent;
+      const date = expenseItem.querySelector('p').textContent;
+      const amount = expenseItem.querySelector('span').textContent;
+
+      const expenseDiv = document.createElement('div');
+      expenseDiv.classList.add('expense-item');
+      expenseDiv.innerHTML = `<span>${date} ${title} ${amount}</span>`;
+      expenseSummaryContainer.appendChild(expenseDiv);
+
+      // price
+      const sumPrice = parseFloat(amount.replace('$', ''));
+      sumTotalAmount += sumPrice;
+    });
+    const totalDiv = document.createElement('div');
+    totalDiv.classList.add('total-amount');
+    totalDiv.innerHTML = `<hr> <h3>Total: $${sumTotalAmount.toFixed(2)}</h3>`;
+    expenseSummaryContainer.appendChild(totalDiv);
+  }
+  else {
+    if (selectedCategory != "default") {
+      // when no items in a category
+      const noExpensesDiv = document.createElement('div');
+      noExpensesDiv.textContent = "No expenses for this category";
+      expenseSummaryContainer.appendChild(noExpensesDiv);
+    }
+  }
+});
+
+
+
